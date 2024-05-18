a1fa15415e00ffded0a2885636ac61f36029889e
Azeem Khan <146321080+azeemkhan1311@users.noreply.github.com>
2024-05-18 03:29:47 -0700

    Update serializers.py

--- shopsy/serializers.py
+++ shopsy/serializers.py
@@ -6,8 +6,3 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Signup
         fields = '__all__'
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
