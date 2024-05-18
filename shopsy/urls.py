ebf2645ec4a7e5c4f9945e41d4efba11a386bb8e
Azeem Khan <146321080+azeemkhan1311@users.noreply.github.com>
2024-05-18 03:35:48 -0700

    Update urls.py

--- shopsy/urls.py
+++ shopsy/urls.py
@@ -6,6 +6,11 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'', views.SignupViewSet)
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("signup", views.signup, name="signup"),
@@ -14,6 +19,8 @@ urlpatterns = [
     path("cart", views.cart_details, name="cart"),
     path("checkout", views.checkout, name="checkout"),
     path("order", views.order, name="order"),
+    path('rest_api/', include(router.urls)),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
